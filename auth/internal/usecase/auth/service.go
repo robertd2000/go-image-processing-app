58b25de39b2d6b42e9427fed4d1002a6a6ec5939
robertd2000 <zdabaghov@mail.ru>
2026-03-01 17:31:01 +0300

    register test + service

--- auth/internal/usecase/auth/service.go
+++ auth/internal/usecase/auth/service.go
@@ -2,23 +2,72 @@
 package auth
 
 import (
-	"github.com/robertd2000/go-image-processing-app/auth/internal/domain/token"
-	"github.com/robertd2000/go-image-processing-app/auth/internal/domain/user"
+	"context"
+	"errors"
+	"fmt"
+
+	tokensDomain "github.com/robertd2000/go-image-processing-app/auth/internal/domain/token"
+	userDomain "github.com/robertd2000/go-image-processing-app/auth/internal/domain/user"
 	"github.com/robertd2000/go-image-processing-app/auth/internal/usecase/auth/port"
 )
 
 type authService struct {
-	userRepo    user.UserRepository
-	refreshRepo token.TokenRepository
+	userRepo    userDomain.UserRepository
+	refreshRepo tokensDomain.TokenRepository
 	hasher      port.PasswordHasher
 	tokenGen    port.TokenGenerator
 }
 
-func NewAuthService(userRepo user.UserRepository, refreshRepo token.TokenRepository, hasher port.PasswordHasher, tokenGen port.TokenGenerator) *authService {
+func NewAuthService(userRepo userDomain.UserRepository, refreshRepo tokensDomain.TokenRepository, hasher port.PasswordHasher, tokenGen port.TokenGenerator) *authService {
 	return &authService{
 		userRepo:    userRepo,
 		refreshRepo: refreshRepo,
 		hasher:      hasher,
 		tokenGen:    tokenGen,
 	}
+}
+
+func (s *authService) Register(ctx context.Context, username, fistname, lastname, email, password string) error {
+	exists, err := s.userRepo.ExistsByEmail(ctx, email)
+	if err != nil {
+		return fmt.Errorf("error by finding user by email")
+	}
+	if exists {
+		return userDomain.ErrUserAlreadyExists
+	}
+
+	hashed, err := s.hasher.Hash(password)
+	if err != nil {
+		if errors.Is(err, userDomain.ErrInvalidPasswordHash) {
+			return err
+		}
+		return fmt.Errorf("hash password: %w", err)
+	}
+
+	user, err := userDomain.CreateUser(username, fistname, lastname, &email, hashed)
+	if err != nil {
+		return err
+	}
+
+	err = s.userRepo.Create(ctx, user)
+	if err != nil {
+		if errors.Is(err, userDomain.ErrUserAlreadyExists) {
+			return err
+		}
+		return fmt.Errorf("create user: %w", err)
+	}
+
+	return nil
+}
+
+func (s *authService) Login(ctx context.Context, email string, password string) (tokensDomain.Tokens, error) {
+	return tokensDomain.Tokens{}, nil
+}
+
+func (s *authService) Refresh(ctx context.Context, refreshToken string) (tokensDomain.Tokens, error) {
+	return tokensDomain.Tokens{}, nil
+}
+
+func (s *authService) Logout(ctx context.Context, refreshToken string) error {
+	return nil
 }
--- auth/internal/usecase/auth/service_test.go
+++ auth/internal/usecase/auth/service_test.go
@@ -1,7 +1,9 @@
 package auth_test
 
 import (
-	"github.com/robertd2000/go-image-processing-app/auth/internal/domain/token"
+	"context"
+
+	tokensDomain "github.com/robertd2000/go-image-processing-app/auth/internal/domain/token"
 	"github.com/robertd2000/go-image-processing-app/auth/internal/domain/user"
 	"github.com/robertd2000/go-image-processing-app/auth/internal/infrastructure/jwt"
 	tokenmem "github.com/robertd2000/go-image-processing-app/auth/internal/infrastructure/persistence/inmemory/token"
@@ -9,16 +11,24 @@ import (
 	"github.com/robertd2000/go-image-processing-app/auth/internal/infrastructure/security"
 	"github.com/robertd2000/go-image-processing-app/auth/internal/usecase/auth"
 	"github.com/robertd2000/go-image-processing-app/auth/internal/usecase/auth/port"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 )
 
 type (
-	AuthService interface{}
+	AuthService interface {
+		Register(ctx context.Context, username, fistname, lastname, email, password string) error
+		Login(ctx context.Context, email string, password string) (tokensDomain.Tokens, error)
+		Refresh(ctx context.Context, refreshToken string) (tokensDomain.Tokens, error)
+		Logout(ctx context.Context, refreshToken string) error
+	}
 
 	AuthTestSuite struct {
+		suite.Suite
 		service   AuthService
 		hasher    port.PasswordHasher
 		userRepo  user.UserRepository
-		tokenRepo token.TokenRepository
+		tokenRepo tokensDomain.TokenRepository
 		tokenGen  port.TokenGenerator
 	}
 )
@@ -30,4 +40,21 @@ func (s *AuthTestSuite) SetupTest() {
 	s.tokenRepo = tokenmem.NewTokenRepository()
 
 	s.service = auth.NewAuthService(s.userRepo, s.tokenRepo, s.hasher, s.tokenGen)
+}
+
+func (s *AuthTestSuite) TestAuthService_Register_Success() {
+	ctx := context.Background()
+	password := "!Secure123"
+	// secure := "!HashedSecure1234"
+	email := "test_user1@example.com"
+	username := "test_user"
+	firstname := "user"
+	lastname := "1"
+
+	err := s.service.Register(ctx, username, firstname, lastname, email, password)
+	assert.NoError(s.T(), err)
+
+	user, err := s.userRepo.GetByEmail(ctx, email)
+	assert.NoError(s.T(), err)
+	assert.NotNil(s.T(), user)
 }
