35005a37e51bd65bad5641acc10158274633ff42
robertd2000 <zdabaghov@mail.ru>
2026-04-01 20:48:54 +0300

    user domain

--- user/go.mod
+++ user/go.mod
@@ -1,3 +1,5 @@
 module github.com/robertd2000/go-image-processing-app/user
 
 go 1.22.0
+
+require github.com/google/uuid v1.6.0
---
+++ user/internal/domain/user/settings.go
@@ -0,0 +1,35 @@
+package user
+
+import "time"
+
+type UserSettings struct {
+	isPublic           bool
+	allowNotifications bool
+	theme              string
+
+	createdAt time.Time
+	updatedAt time.Time
+}
+
+func NewSettings() *UserSettings {
+	now := time.Now()
+
+	return &UserSettings{
+		isPublic:           true,
+		allowNotifications: true,
+		theme:              "light",
+		createdAt:          now,
+		updatedAt:          now,
+	}
+}
+
+func (s *UserSettings) Update(
+	isPublic bool,
+	allowNotifications bool,
+	theme string,
+) {
+	s.isPublic = isPublic
+	s.allowNotifications = allowNotifications
+	s.theme = theme
+	s.updatedAt = time.Now()
+}
