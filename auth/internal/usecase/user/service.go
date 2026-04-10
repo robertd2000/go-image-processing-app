8ad190b05b3ff0ebe6f7a4ceb45f87617583272a
robertd2000 <zdabaghov@mail.ru>
2026-04-10 20:25:06 +0300

    UpdateStatus

--- auth/internal/usecase/user/service.go
+++ auth/internal/usecase/user/service.go
@@ -2,6 +2,7 @@ package user
 
 import (
 	"context"
+	"errors"
 
 	"github.com/google/uuid"
 	userDomain "github.com/robertd2000/go-image-processing-app/auth/internal/domain/user"
@@ -18,5 +19,27 @@ func NewUserSyncService(userRepo userDomain.UserRepository) *userSyncService {
 }
 
 func (s *userSyncService) UpdateStatus(ctx context.Context, userID uuid.UUID, status string) error {
+	if userID == uuid.Nil {
+		return userDomain.ErrInvalidUserID
+	}
+
+	switch status {
+	case "active", "inactive":
+	default:
+		return userDomain.ErrInvalidUserStatus
+	}
+
+	user, err := s.userRepo.GetByID(ctx, userID)
+	if err != nil {
+		if errors.Is(err, userDomain.ErrUserNotFound) {
+			return nil
+		}
+		return err
+	}
+
+	if user.Status() == status {
+		return nil
+	}
+
 	return s.userRepo.UpdateStatus(ctx, userID, status)
 }
