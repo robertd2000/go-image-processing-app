c3261f5ebf35f6dfa09dc67967c6bdda8eb85b38
robertd2000 <zdabaghov@mail.ru>
2026-04-27 10:50:58 +0300

    UploadImageOutput

--- image/internal/usecase/image/service_test.go
+++ image/internal/usecase/image/service_test.go
@@ -1,1 +1,11 @@
 package image_test
+
+import (
+	"context"
+
+	"github.com/robertd2000/go-image-processing-app/image/internal/usecase/image/model"
+)
+
+type ImageService interface {
+	UploadImage(ctx context.Context, input model.UploadImageInput) (*model.UploadImageOutput, error)
+}
