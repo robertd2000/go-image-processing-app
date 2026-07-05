e9a4f3e02c421c916198018f79eb4d162f31f30f
robertd2000 <turgamobil@gmail.com>
2026-07-05 15:06:40 +0300

    transformRepository

--- processor/internal/infrastructure/persistence/postgtres/transformation/transformation.go
+++ processor/internal/infrastructure/persistence/postgtres/transformation/transformation.go
@@ -1,1 +1,21 @@
 package transformation
+
+import (
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/robertd2000/go-image-processing-app/processor/internal/port"
+	"go.uber.org/zap"
+)
+
+type transformRepository struct {
+	db      *pgxpool.Pool
+	logger  *zap.SugaredLogger
+	metrics port.Metrics
+}
+
+func NewTransformRepository(db *pgxpool.Pool, logger *zap.SugaredLogger, metrics port.Metrics) *transformRepository {
+	return &transformRepository{
+		db:      db,
+		logger:  logger,
+		metrics: metrics,
+	}
+}
