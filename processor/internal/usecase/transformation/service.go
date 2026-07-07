6419137628fd79d675b302dd547f3f459e2db40a
robertd2000 <turgamobil@gmail.com>
2026-07-07 19:54:44 +0300

    NewTransformationService

--- processor/internal/usecase/transformation/service.go
+++ processor/internal/usecase/transformation/service.go
@@ -3,10 +3,29 @@ package transformation
 import (
 	transformDomain "github.com/robertd2000/go-image-processing-app/processor/internal/domain/transformation"
 	"github.com/robertd2000/go-image-processing-app/processor/internal/port"
+	"go.uber.org/zap"
 )
 
-type Service struct {
+type transformationService struct {
 	transformRepo transformDomain.Repository
 	storage       port.Storage
 	txManager     port.TxManager
+	logger        *zap.SugaredLogger
+	metrics       port.Metrics
+}
+
+func NewTransformationService(
+	transformRepo transformDomain.Repository,
+	storage port.Storage,
+	txManager port.TxManager,
+	logger *zap.SugaredLogger,
+	metrics port.Metrics,
+) *transformationService {
+	return &transformationService{
+		transformRepo: transformRepo,
+		storage:       storage,
+		txManager:     txManager,
+		logger:        logger,
+		metrics:       metrics,
+	}
 }
